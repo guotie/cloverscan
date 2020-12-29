ecb94481a4a95cffb27c825efa782c2bdfb82e24
guoite <guotie.9@gmail.com>
2020-12-29 21:40:15 +0800

    remove build

--- build/btc/address.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-var BTCAdress = /** @class */ (function () {
-    function BTCAdress() {
-    }
-    return BTCAdress;
-}());
-exports.default = BTCAdress;
